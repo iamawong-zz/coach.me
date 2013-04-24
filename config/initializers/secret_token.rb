1e54064f8067bc13bb9ffa9174cd37ae7a6f649d
Andrew Wong <iamawong@outlook.com>
2013-04-23 19:10:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoachMe::Application.config.secret_token = 'd032c6e108c20bb760f8dae4f14c12c28644631194e1ff12bf4d1dcf08307ed9adfdb01983e924ec3d9d51b608c9e9a288f2e958cc0b468ddd6090d187216e92'
