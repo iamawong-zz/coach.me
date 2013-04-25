a9fbaac3e40ab6153698e5c229dc6d5d22a52080
Andrew Wong <iamawong@outlook.com>
2013-04-24 22:49:03 -0700

    Heroku configs application.rb

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module CoachMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Something needed for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
